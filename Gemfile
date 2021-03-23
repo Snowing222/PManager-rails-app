2d9973d0baf25b7cfe5eac3d2a453e6deffb216a
snowing222 <snowing222@gmail.com>
2021-03-23 12:37:43 -0400

    testing

--- Gemfile
+++ Gemfile
@@ -8,6 +8,9 @@ gem 'rails', '~> 6.0.3', '>= 6.0.3.4'
 # Use sqlite3 as the database for Active Record
 gem 'pg'
 gem 'rails_12factor', group: :production
+gem 'omniauth-rails_csrf_protection'
+
+
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   OmniAuth.config.allowed_request_methods = [:post, :get]
-  provider :google_oauth2, ENV['GOOGLECLIENTID'], ENV['GOOGLECLIENTSECRET']
+  provider :google_oauth2, ENV['GOOGLECLIENTID'], ENV['GOOGLECLIENTSECRET'], { scope: 'userinfo.email,userinfo.profile,https://www.google.com/m8/feeds', access_type: 'offline', approval_prompt: '', client_options: {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}} }
 end
 
 
